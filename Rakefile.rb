b01663557a691b64b8e126feb86d497865fd26d6
Gustavo Cavero <caverogustavo@hotmail.com>
2017-12-08 08:52:57 -0400

    Actualizando comentarios del archivo rake

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,8 +4,9 @@ config = YAML.load_file('config/env.yml')
 report_path = config['report']['report_path']
 report_title = config['report']['title']
 
-# Task to execute a feature with specified tag and have report builder Report
-# @param tag, String, Represent the tag name to execute
+# Tarea para ejeccutar un feature con un tag especifico y obtener el reporte customizado por report_builder
+# @param tag, String, representa el tag que sera ejecutado
+# @example, rake execute[admision]
 task :execute, [:tag] do |_t, arg|
   system "cucumber -f json -o #{report_path}/#{arg[:tag]}.json -f html -o #{report_path}/"\
          "#{arg[:tag]}.html -t @#{arg[:tag]}"
@@ -14,6 +15,10 @@ end
 
 # Task to execute a feature with specified tag and have basic cucumber report
 # @param tag, String, Represent the tag name to execute
+
+# Tarea para ejeccutar un feature con un tag especifico y obtener el reporte nativo de cucumber
+# @param tag, String, representa el tag que sera ejecutado
+# @example, rake run[admision]
 task :run, [:tag] do |_t, arg|
   system "cucumber -f json -o #{report_path}/#{arg[:tag]}.json -f html -o #{report_path}/"\
          "#{arg[:tag]}.html -t @#{arg[:tag]}"
